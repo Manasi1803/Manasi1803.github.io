d048fdebadff1af84f3cca27810ba5be8b43ee20
Manasi_Sherkhane <manasisherkhane34@gmail.com>
2020-12-19 18:29:15 +0530

    updates

--- Homepage/index.js
+++ Homepage/index.js
@@ -5,3 +5,12 @@ $(".rmb").click(function(){
      return v === 'Show More' ? 'Show Less' : 'Show More'
   });
   });
+
+// date picker
+  $ ('#cale').click(function() {
+    $(this).datepicker({ 
+          autoclose: true, 
+          todayHighlight: true,
+          format: "dd M yyyy"
+    }).datepicker('update', new Date());
+  });
